c6ced995f031c85cf200eb18ad1b168d66875189
Christian <3003html@gmail.com>
2022-07-13 07:33:37 -0500

    Case inProgress false created

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ExpensiveFunction(n int) int {
-	fmt.Printf("Calculating expensive function for %d\n", n)
+	//fmt.Printf("Calculating expensive function for %d\n", n)
 	time.Sleep(5 * time.Second)
 	return n
 }
@@ -29,9 +29,27 @@ func (s *Service) Work(job int) {
 		s.IsPending[job] = append(s.IsPending[job], response)
 		s.Mutex.Unlock()
 		fmt.Printf("Waiting for Response job: %d\n", job)
-		//
 		resp := <-response
 		fmt.Printf("Response Done, received %d\n", resp)
 		return
 	}
+	s.Mutex.RUnlock()
+	s.Mutex.Lock()
+	s.InProgress[job] = true
+	s.Mutex.Unlock()
+	fmt.Printf("Calculating expensive function for %d\n", job)
+	result := ExpensiveFunction(job)
+	s.Mutex.RLock()
+	pendingWorkers, exist := s.IsPending[job]
+	s.Mutex.RUnlock()
+	if exist {
+		for _, pendingWorker := range pendingWorkers {
+			pendingWorker <- result
+		}
+		fmt.Printf("Result send, all pending workers ready for job: %d\n", job)
+	}
+	s.Mutex.Lock()
+	s.InProgress[job] = false
+	s.IsPending[job] = make([]chan int, 0)
+	s.Mutex.Unlock()
 }
