73c8e5d98474cf641af0fc40da34df019145e3d0
Christian <3003html@gmail.com>
2022-07-13 07:01:53 -0500

    Expensive Function created

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func ExpensiveFunction() {
+import (
+	"fmt"
+	"time"
+)
 
+func ExpensiveFunction(n int) int {
+	fmt.Printf("Calculating expensive function for %d\n", n)
+	time.Sleep(5 * time.Second)
+	return n
 }
