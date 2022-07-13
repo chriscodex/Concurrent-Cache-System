fe15639d8856be3807804b9e6fd478aeba7eacd6
Christian <3003html@gmail.com>
2022-07-13 07:24:08 -0500

    Case InProgress true created

--- main.go
+++ main.go
@@ -15,5 +15,23 @@ func ExpensiveFunction(n int) int {
 type Service struct {
 	InProgress map[int]bool
 	IsPending  map[int][]chan int
-	Lock       sync.RWMutex
+	Mutex      sync.RWMutex
+}
+
+func (s *Service) Work(job int) {
+	s.Mutex.RLock()
+	exist := s.InProgress[job]
+	if exist {
+		s.Mutex.Unlock()
+		response := make(chan int)
+		defer close(response)
+		s.Mutex.Lock()
+		s.IsPending[job] = append(s.IsPending[job], response)
+		s.Mutex.Unlock()
+		fmt.Printf("Waiting for Response job: %d\n", job)
+		//
+		resp := <-response
+		fmt.Printf("Response Done, received %d\n", resp)
+		return
+	}
 }
