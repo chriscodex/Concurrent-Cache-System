694091db379f8cddd8a0dac8e9f67904f9fa6099
Christian <3003html@gmail.com>
2022-07-13 07:07:58 -0500

    struct Service created

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,4 +10,10 @@ func ExpensiveFunction(n int) int {
 	fmt.Printf("Calculating expensive function for %d\n", n)
 	time.Sleep(5 * time.Second)
 	return n
+}
+
+type Service struct {
+	InProgress map[int]bool
+	IsPending  map[int][]chan int
+	Lock       sync.RWMutex
 }
